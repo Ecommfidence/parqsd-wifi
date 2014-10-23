7883c2bd36bb2724313cd5e9d67d473466441e5a
Pablo Garcia <pgarciacamou@gmail.com>
2014-10-23 16:06:17 -0700

    Removed livereload: too slow..

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,8 @@ module.exports = function (grunt){
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	// grunt.loadNpmTasks('grunt-customize-bootstrap');
 	// grunt.loadNpmTasks('');
 
 	grunt.initConfig({
@@ -15,18 +17,18 @@ module.exports = function (grunt){
 				,spawn: false
 			}
 			,build: {
-				options: {
-					livereload: 35720 // port
-				}
-				,files: ['src/**/*.js', 'src/**/*.jade', 'tests/**/*.spec.js', 'Gruntfile.js', 'package.json', '!**/node_modules/**']
+				files: ['src/**/*.js', 'src/**/*.jade', 'src/**/*.styl', 'tests/**/*.spec.js', 'Gruntfile.js', 'package.json', '!**/node_modules/**']
 				,tasks: ['build']
+				// ,options: {
+				// 	livereload: 35720 // port
+				// }
 			}
 			,karma: {
-				options: {
-					livereload: 35721 // port
-				}
-				,files: ['src/**/*.js', 'src/**/*.jade', 'tests/**/*.spec.js', 'Gruntfile.js', 'karma.conf.js', '!**/node_modules/**']
+				files: ['src/**/*.js', 'src/**/*.jade', 'src/**/*.styl', 'tests/**/*.spec.js', 'Gruntfile.js', 'karma.conf.js', '!**/node_modules/**']
 				,tasks: ['unitTests']
+				// ,options: {
+				// 	livereload: 35721 // port
+				// }
 			}
 		}
 		,clean: {
@@ -55,6 +57,8 @@ module.exports = function (grunt){
 				}
 				,files: {
 					'build/index.html': 'src/index.jade'
+					,'build/wifi.html': 'src/wifi.ptl.jade'
+					,'build/done.html': 'src/done.ptl.jade'
 				}
 			}
 			,dist: {
@@ -66,6 +70,8 @@ module.exports = function (grunt){
 				}
 				,files: {
 					'dist/index.html': 'src/index.jade'
+					,'dist/wifi.html': 'src/wifi.ptl.jade'
+					,'dist/done.html': 'src/done.ptl.jade'
 				}
 			}
 			// ,ghpages: { //gh-pages Branch
@@ -92,7 +98,10 @@ module.exports = function (grunt){
 				,files: {
 					'build/index.min.js': [
 						'src/index.module.js'//
+						,'src/index.routes.js'
 						,'src/index.ctrl.js'
+						,'src/wifi.ctrl.js'
+						,'src/done.ctrl.js'
 					]//
 				}
 			}
@@ -104,6 +113,8 @@ module.exports = function (grunt){
 					'dist/index.min.js': [
 						'src/index.module.js'//
 						,'src/index.ctrl.js'
+						,'src/wifi.ctrl.js'
+						,'src/done.ctrl.js'
 					]//
 				}
 			}
@@ -117,10 +128,14 @@ module.exports = function (grunt){
 					,src: [
 						'vendor/angular/v1.3.0/angular.min.js'//
 						,'vendor/angular/v1.3.0/angular.min.js.map'
+						,'vendor/angular/v1.3.0/angular-route.min.js'
+						,'vendor/angular/v1.3.0/angular-route.min.js.map'
 						,'vendor/jquery/v2.1.1/jquery.min.js'
 						,'vendor/jquery/v2.1.1/jquery.min.map'
 						,'vendor/bootstrap/v3.2.0/dist/css/bootstrap.min.css'
 						,'vendor/bootstrap/v3.2.0/dist/js/bootstrap.min.js'
+						,'vendor/fontawesome/v4.2.0/css/font-awesome.min.css'
+						,'vendor/fontawesome/v4.2.0/fonts/*'
 						,'images/*'
 					]//
 				}]
@@ -133,21 +148,47 @@ module.exports = function (grunt){
 					,src: [
 						'vendor/angular/v1.3.0/angular.min.js'//
 						,'vendor/angular/v1.3.0/angular.min.js.map'
+						,'vendor/angular/v1.3.0/angular-route.min.js'
+						,'vendor/angular/v1.3.0/angular-route.min.js.map'
 						,'vendor/jQuery/v2.1.1/jquery.min.js'
 						,'vendor/jQuery/v2.1.1/jquery.min.map'
 						,'vendor/Bootstrap/v3.2.0/css/bootstrap.min.css'
 						,'vendor/Bootstrap/v3.2.0/js/bootstrap.min.js'
+						,'vendor/fontawesome/v4.2.0/css/font-awesome.min.css'
+						,'vendor/fontawesome/v4.2.0/fonts/*'
 						,'images/*'
 					]//
 				}]
 			}
 		}
+		,stylus: {
+			build: {
+				files: {
+					'build/index.css': ['src/index.styl']
+				}
+			}
+			,dist: {
+				files: {
+					'dist/index.css': ['src/index.styl']
+				}
+			}
+		}
+		// ,'customize-bootstrap': {
+		// 	build: {
+		// 		options: {
+		// 			bootstrapPath: 'vendor/bootstrap/v3.2.0',
+		// 			src: 'build/vendor/bootstrap/v3.2.0',
+		// 			dest: 'src/styles/',
+		// 		}
+		// 	}
+		// }
+
 	});
 
 	grunt.registerTask('unitTests', ['karma:unit:run']);//
-	grunt.registerTask('build', ['copy:build', 'uglify:build', 'jade:build']);
-	grunt.registerTask('dist', ['copy:dist', 'uglify:dist', 'jade:dist', 'jade:ghpages'])
-	grunt.registerTask('initTests', ['karma:unit', 'watch:karma'])
+	grunt.registerTask('build', ['copy:build', 'uglify:build', 'jade:build', 'stylus:build']);
+	grunt.registerTask('dist', ['copy:dist', 'uglify:dist', 'jade:dist', 'stylus:dist']);//'jade:ghpages', 
+	grunt.registerTask('initTests', ['karma:unit', 'watch:karma']);
 	
 	grunt.registerTask('default', ['watch:build']);
 };
