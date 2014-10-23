5153e8cbe7104cafa4fa578d6d48495075fe69cc
Pablo Garcia <pgarciacamou@gmail.com>
2014-10-23 11:01:10 -0700

    CORS Post technique added

--- src/index.ctrl.js
+++ src/index.ctrl.js
@@ -5,25 +5,35 @@ angular.module('index')
 	$scope.errorInEmail = true;
 
 	$scope.$watch('email', function (newVal, oldVal){
-		console.log(arguments);
 		$scope.errorInEmail = newVal && newVal.length > 0 && !/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(newVal);
-		console.log('hola')
 	});
 
 	$scope.fetch = function (){
-		if()
-		$http.post('//parqsd.us8.list-manage.com/subscribe/post?u=ad0cc5107731b466cd161f702&amp;id=738d9979d0', {
-			FNAME:$scope.fname
-			,LNAME:$scope.lname
-			,EMAIL:$scope.email
-			,'b_ad0cc5107731b466cd161f702_738d9979d0':''
-			,subscribe:'Subscribe'
-		})
-		.success(function onSuccess(data, status, headers, config){
-			$scope.results = response.data.results;
-		})
-		.error(function onError(data, status, headers, config){
-			console.log('adios');
-		});
-	};
-}]);
+		if($scope.fname && $scope.fname.length > 0 && $scope.lname && $scope.lname.length > 0 && newVal && newVal.length > 0 && !$scope.errorInEmail)
+			(function crossDomainPost() {
+			  // Add the iframe with a unique name
+			  var iframe = document.createElement("iframe");
+		  	var uniqueString = '398df374-325d-4bc9-b750-a54e57e0ab44';//"CHANGE_THIS_TO_SOME_UNIQUE_STRING";
+		  	document.body.appendChild(iframe);
+		  	iframe.style.display = "none";
+		  	iframe.contentWindow.name = uniqueString;
+
+		  	// construct a form with hidden inputs, targeting the iframe
+		  	var form = document.createElement("form");
+		  	form.target = uniqueString;
+		  	form.action = '//parqsd.us8.list-manage.com/subscribe/post?u=ad0cc5107731b466cd161f702&amp;id=738d9979d0';
+		  	form.method = 'POST';
+
+		  	// repeat for each parameter
+		  	var input = document.createElement("input");
+		  	input.type = "hidden";
+		  	input.name = 'EMAIL';
+		  	input.value = $scope.email;
+		  	form.appendChild(input);
+
+		  	document.body.appendChild(form);
+		  	form.submit();
+		  	document.body.removeChild(form);
+		  })();
+		};
+	}]);
